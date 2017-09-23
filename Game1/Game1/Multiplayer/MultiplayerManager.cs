947cc871ccc34bb29a651c53f5f0ae2d0d956855
Bueno <celestin.metral@gmail.com>
2017-09-23 19:52:46 +0200

    ça marche online

--- Game1/Game1/Multiplayer/MultiplayerManager.cs
+++ Game1/Game1/Multiplayer/MultiplayerManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -22,7 +23,7 @@ namespace DowerTefenseGame.Multiplayer
             try
             {
                 // Client TCP
-                TcpClient connectionClient = new TcpClient(connectionServerIP, connectionServerPort);
+                TcpClient connectionClient = new TcpClient(IPAddress.Any.ToString(), connectionServerPort);
                 // Récupération du flux
                 NetworkStream ns = connectionClient.GetStream();
 
