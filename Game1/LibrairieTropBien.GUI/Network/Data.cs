1565883dbc32e65b5026cf7969b21458702b73a5
Bueno <celestin.metral@gmail.com>
2017-09-24 00:34:57 +0200

    debut d'envoi d'objets...

---
+++ Game1/LibrairieTropBien.GUI/Network/Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace LibrairieTropBien.GUI.Network
+{
+    /// <summary>
+    /// Classe d'échange de donnée
+    /// </summary>
+    public class Data
+    {
+        /// <summary>
+        /// Type de l'objet
+        /// </summary>
+        public Type type;
+        /// <summary>
+        /// Données à échanger
+        /// </summary>
+        public Stream stream;
+
+        /// <summary>
+        /// Données à échanger sur le réseau
+        /// </summary>
+        public Data()
+        {
+
+        }
+
+        /// <summary>
+        /// Données à échanger sur le réseau
+        /// </summary>
+        public Data(Type _type, string _data)
+        {
+            this.type = _type;
+            this.stream = _data;
+        }
+    }
+}
---
+++ Game1/LibrairieTropBien.GUI/Network/ObjectSender.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Xml.Serialization;
+
+namespace LibrairieTropBien.GUI.Network
+{
+    /// <summary>
+    /// Classe pour envoyer des objets sur le réseau
+    /// </summary>
+    public static class ObjectSender
+    {
+        /// <summary>
+        /// Envoi d'un objet
+        /// </summary>
+        /// <param name="_item">Objet à envoyer</param>
+        /// <param name="_tcpClient">TCPClient</param>
+        /// <returns>Succès de l'opéarion</returns>
+        public static bool Send(object _item, TcpClient _tcpClient)
+        {
+            // Succès de l'opération
+            bool success = false;
+
+            // Sérialisation selon le type
+            var objectSerializer = new XmlSerializer(_item.GetType());
+
+            // Passage en donnée
+            Data data = new Data()
+            {
+                type = _item.GetType(),
+            };
+            objectSerializer.Serialize(data.stream, _item);
+
+            // Récupération du flux réseau
+            var dataSerializer = new XmlSerializer(typeof(Data));
+            var networkStream = _tcpClient.GetStream();
+
+            // Écriture du flux
+            if (networkStream.CanWrite)
+            {
+                dataSerializer.Serialize(networkStream, data);
+                success = true;
+            }
+
+            // Retour
+            return success;
+        }
+
+        public static object Receive(NetworkStream _stream)
+        {
+            // Serialiseur de type Data
+            XmlSerializer dataSerializer = new XmlSerializer(typeof(Data));
+            // Récupération de l'objet Data
+            Data receivedData = (Data)dataSerializer.Deserialize(_stream);
+
+            // Type de l'objet à recevoir
+            Type type = receivedData.type.GetType();
+            // Serialiseur du type de l'objet
+            XmlSerializer objectSerialize = new XmlSerializer(type);
+            // Récupération de l'objet
+
+
+            return  ;
+        }
+    }
+}
